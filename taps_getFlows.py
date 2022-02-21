1d6a2767debffefc54e2a4cdf75c3629db99ecbb
Vytex0 <Vytex0@gmail.com>
2022-02-21 18:08:54 +0100

    getFlows packets length calculation

--- taps_getFlows.py
+++ taps_getFlows.py
@@ -23,7 +23,7 @@ def convertSecondsToPrintableTime(seconds):
         return str(int(seconds))+"s"
     if(seconds < 3600):
         return str(int(seconds//60))+"m "+str(int(seconds%60))+"s"
-    return str(int(seconds//3600))+"h "+str(int((seconds%3600)%60))+"m "+str(int(seconds%60))+"s"
+    return str(int(seconds//3600))+"h "+str(int((seconds%3600)//60))+"m "+str(int(seconds%60))+"s"
 
 def getPacketTime(packet):
     return float(packet.time)
